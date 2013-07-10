7f10bc295758c560cee04babdabe6a5d1b9247b3
thebiologist13 <thebiologist13@gmail.com>
2013-07-09 20:12:36 -0600

    Added fromName(String name) to Operation.

--- attributelib/src/main/java/com/github/thebiologist13/attributelib/Operation.java
+++ attributelib/src/main/java/com/github/thebiologist13/attributelib/Operation.java
@@ -2,14 +2,16 @@ package com.github.thebiologist13.attributelib;
 
 public enum Operation {
 
-	INCREMENT(0),
-	ADDITIVE(1),
-	MULTIPLICATIVE(2);
+	INCREMENT(0, "Increment"),
+	ADDITIVE(1, "Additive"),
+	MULTIPLICATIVE(2, "Multiplicative");
 	
 	private final int OP;
+	private final String NAME;
 	
-	Operation(int op) {
+	Operation(int op, String name) {
 		this.OP = op;
+		this.NAME = name;
 	}
 	
 	public static Operation fromId(int id) {
@@ -22,6 +24,18 @@ public enum Operation {
 			return MULTIPLICATIVE;
 		}
 		return null;
+	}
+	
+	public static Operation fromName(String name) {
+		if(name.equalsIgnoreCase(INCREMENT.NAME)) {
+			return INCREMENT;
+		} else if(name.equalsIgnoreCase(ADDITIVE.NAME)) {
+			return ADDITIVE;
+		} else if(name.equalsIgnoreCase(MULTIPLICATIVE.NAME)) {
+			return MULTIPLICATIVE;
+		} else {
+			return null;
+		}
 	}
 	
 	public int getOperation() {
